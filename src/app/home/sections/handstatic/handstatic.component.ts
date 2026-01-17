adffa0718db2ed46c3253eb16fe9690bc5b19391
rahulKarnati181 <rahulkarnati181@gmail.com>
2026-01-17 15:47:29 +0530

    picture some done

--- src/app/home/sections/handstatic/handstatic.component.ts
+++ src/app/home/sections/handstatic/handstatic.component.ts
@@ -1,13 +1,6 @@
-// creative-process.component.ts
-import { AfterViewInit, Component, ElementRef, OnDestroy, QueryList, ViewChild, ViewChildren } from "@angular/core";
+// handstatic.ts
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterLink } from "@angular/router";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-
 
 @Component({
   selector: "app-hand-static",
@@ -17,13 +10,13 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrls: ["./handstatic.css"],
 })
 export class handstatic {
+  @ViewChild("phoneVid") phoneVid!: ElementRef<HTMLVideoElement>;
 
-    @ViewChild('phoneVid') phoneVid!: ElementRef<HTMLVideoElement>;
-    ngAfterViewInit(){
-  const v = this.phoneVid?.nativeElement;
-  if (!v) return;
-  v.muted = true;
-  v.playsInline = true;
-  v.play().catch(() => {});
-}
+  ngAfterViewInit() {
+    const v = this.phoneVid?.nativeElement;
+    if (!v) return;
+    v.muted = true;
+    v.playsInline = true;
+    v.play().catch(() => {});
+  }
 }
